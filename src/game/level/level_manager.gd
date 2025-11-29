d7892844830a466e64e60dce99eee4692d91643b
Aiden Moosa <aiden.moosa@gmail.com>
2025-11-29 23:17:37 +0000

    Pause button
    And remove old tower buttons

--- src/game/level/level_manager.gd
+++ src/game/level/level_manager.gd
@@ -12,7 +12,7 @@ var enemies_on_screen := 0
 @export var enemy_spawner : Node
 @export var player_money : int
 @export var wave_start_button : Button
-@export var place_towers_button : Button
+@export var pause_button : Button
 @export var player_health: Label
 
 # DEBUG LIKELY (will be better later)
@@ -34,7 +34,8 @@ func _ready():
 	debug_spawn_worms()
 	
 	wave_start_button.pressed.connect(on_wave_start_button_pressed)
-	place_towers_button.pressed.connect(on_place_towers_button_pressed)
+	pause_button.pressed.connect(on_pause_button_pressed)
+	
 	worms.text = "WORMS: " + str(player_worms)
 	player_money_label.text = "$" + str(player_money)
 	
@@ -127,5 +128,9 @@ func on_wave_start_button_pressed():
 	if between_waves:
 		start_wave()
 		
-func on_place_towers_button_pressed():
-	$"../TowerPlacement".start()
+func on_pause_button_pressed():
+	var time_scale = Engine.time_scale
+	if time_scale <= 0:
+		Engine.time_scale = 1
+	else:
+		Engine.time_scale = 0
