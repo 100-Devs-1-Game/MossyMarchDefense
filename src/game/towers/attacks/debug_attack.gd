922316c65207be239cd8c8ad72b11eb9f826cdb7
Aiden Moosa <aiden.moosa@gmail.com>
2025-12-11 21:09:18 +0000

    Tower attack refactor
    Move attack code out of the debug tower and rename to base tower. Attacks can be spawned per tower type, as long as they have the properties attack_interval, attack_marker, and the shoot(target) method.

---
+++ src/game/towers/attacks/debug_attack.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@onready var bullet = load("res://game/towers/attacks/debug_projectile.tscn")
+
+@export var attack_source : Marker2D
+
+var attack_interval = 1.5
+
+var bulletDamage = 10000
+
+func shoot(currTarget: Node2D):
+	if currTarget == null:
+		return
+		
+	var spawn_position = attack_source.global_position
+	var look_direction = spawn_position.angle_to_point(currTarget.global_position)
+	look_direction += PI/2
+	
+	var instance = bullet.instantiate()
+	instance.dir = look_direction
+	instance.spawnPos = spawn_position
+	instance.spawnRot = look_direction
+	instance.base_damage = bulletDamage
+	get_tree().current_scene.add_child.call_deferred(instance)
--- src/game/towers/base_tower.gd
+++ src/game/towers/base_tower.gd
@@ -1,48 +1,41 @@
 extends StaticBody2D
 
-@onready var bullet = load("res://game/towers/debug_projectile.tscn")
-
 @export var tower_data : TowerData
 
-var bulletDamage = 10000
 var currTargets = []
 var currTarget = null
-var shootInterval = 1.5
-var shootTimer = 0.0
+
+var attack : Node2D
+var attackTimer : float
+
+var attack_dictionary = {
+	GlobalEnums.TowerType.PlantPot: preload("res://game/towers/attacks/debug_attack.tscn"),
+	GlobalEnums.TowerType.WateringCan: preload("res://game/towers/attacks/debug_attack.tscn"),
+}
 
 func _ready():
 	if tower_data:
 		$DetectionArea/CollisionShape2D.shape.radius = tower_data.detection_radius
 		$TowerSprite.sprite_frames = tower_data.sprite_frames
 		$TowerSprite.play()
+		
+		attack = attack_dictionary[tower_data.tower_type].instantiate()
+		attack.attack_source = $AttackSource
+		attackTimer = attack.attack_interval
+		add_child.call_deferred(attack)
 	
 	$DetectionArea.body_entered.connect(_enter_tower_range)
 	$DetectionArea.body_exited.connect(_exit_tower_range)
 
 func _process(delta):
 	if currTarget != null:
-		shootTimer -= delta
-		if shootTimer <= 0.0:
-			shoot()
-			shootTimer = shootInterval
+		attackTimer -= delta
+		if attackTimer <= 0.0:
+			attack.shoot(currTarget)
+			attackTimer = attack.attack_interval
 	else:
 		if currTargets.size() > 0:
 			currTarget = currTargets[0]
-
-func shoot():
-	if currTarget == null:
-		return
-		
-	var spawn_position = $AttackSource.global_position
-	var look_direction = spawn_position.angle_to_point(currTarget.global_position)
-	look_direction += PI/2
-	
-	var instance = bullet.instantiate()
-	instance.dir = look_direction
-	instance.spawnPos = spawn_position
-	instance.spawnRot = look_direction
-	instance.base_damage = bulletDamage
-	get_tree().current_scene.add_child.call_deferred(instance)
 
 func _enter_tower_range(body):
 	if body.is_in_group("enemy") and body.enemy_type != GlobalEnums.EnemyType.Worm:
